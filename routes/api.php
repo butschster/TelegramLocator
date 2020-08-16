a43936d1dcdbf4127cc67589710ab132599caa50
butschster <butschster@gmail.com>
2020-08-16 13:09:42 +0300

    - Исправлен route для получения списка точек

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,5 @@
 use App\Models\Room;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/points/{room}', function (Room $room) {
-    return Room\Point::where('room_uuid', $room->uuid)
-        ->latest('updated_at')
-        ->where('updated_at', '>', now()->subDay())
-        ->get()
-        ->map(function (Room\Point $point) {
-            return [
-                'lat' => $point->location->getLat(),
-                'lng' => $point->location->getLng(),
-                'username' => $point->username,
-                'created_at' => $point->updated_at
-            ];
-        });
-})->name('room.points');
+Route::get('/points/{room}', 'TelegramController@getPointsForLastDay')
+    ->name('room.points');
